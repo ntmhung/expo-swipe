a90ec9cdc8ab689a399762c0a4543805ce8c3f98
Hung.Nguyen <ntmhung94@gmail.com>
2018-06-25 23:34:54 +0700

    Render card item

---
+++ src/Deck.js
@@ -0,0 +1,23 @@
+/**
+ * Created by minhhung on 6/24/18.
+ */
+import React, {Component} from "react";
+import {View, Animated} from "react-native";
+
+class Deck extends Component {
+    renderCards(){
+        return this.props.data.map(item => {
+            return this.props.renderCard(item);
+        })
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        )
+    }
+}
+
+export default Deck;
