155aa8ab2edaa37d733e8d20df6bbe9d7939e24f
Hung.Nguyen <ntmhung94@gmail.com>
2018-06-28 23:32:12 +0700

    moving first card item with gesture

--- src/Deck.js
+++ src/Deck.js
@@ -2,18 +2,65 @@
  * Created by minhhung on 6/24/18.
  */
 import React, {Component} from "react";
-import {View, Animated} from "react-native";
+import {View, Animated, PanResponder} from "react-native";
 
 class Deck extends Component {
-    renderCards(){
-        return this.props.data.map(item => {
+    constructor(props) {
+        super(props);
+
+        const position = new Animated.ValueXY();
+
+        const panResponder = PanResponder.create({
+            /**
+             * Execute anytime user taps on the screen.
+             * If return TRUE, the whole instance of PanResponder will be responsible for pressing on the screen
+             * @return Boolean
+             */
+            onStartShouldSetPanResponder: () => true,
+
+            /**
+             * Execute anytime user drag finger around the screen.
+             */
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({x: gesture.dx, y: gesture.dy})
+            },
+
+            /**
+             * Execute anytime user move finger out of screen.
+             */
+            onPanResponderRelease: () => {
+
+            }
+
+        });
+
+        //not necessary to do this
+        this.state = {panResponder, position};
+    }
+
+    renderCards() {
+        return this.props.data.map((item, index) => {
+            if(index == 0){
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style=style={this.state.position.getLayout()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
             return this.props.renderCard(item);
         })
     }
 
     render() {
         return (
-            <View>
+            <View
+                style=style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers}
+            >
                 {this.renderCards()}
             </View>
         )
