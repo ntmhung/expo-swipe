42a2e0e4915c4a7b9557b10222290cc0fb7818e8
Hung.Nguyen <ntmhung94@gmail.com>
2018-07-01 16:34:02 +0700

    add swipeLeft and swipeRight handler

--- src/Deck.js
+++ src/Deck.js
@@ -2,7 +2,11 @@
  * Created by minhhung on 6/24/18.
  */
 import React, {Component} from "react";
-import {View, Animated, PanResponder} from "react-native";
+import {View, Animated, PanResponder, Dimensions} from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 400;
 
 class Deck extends Component {
     constructor(props) {
@@ -28,8 +32,14 @@ class Deck extends Component {
             /**
              * Execute anytime user move finger out of screen.
              */
-            onPanResponderRelease: () => {
-
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
             }
 
         });
@@ -38,13 +48,46 @@ class Deck extends Component {
         this.state = {panResponder, position};
     }
 
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction) {
+        const {onSwipeRight, onSwipeLeft} = this.props;
+        direction === 'right' ? onSwipeRight() : onSwipeLeft();
+    }
+
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: {x: 0, y: 0}
+        }).start();
+    }
+
+    setCardGestureStyle() {
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{rotate}]
+        }
+    }
+
     renderCards() {
         return this.props.data.map((item, index) => {
-            if(index == 0){
+            if (index == 0) {
                 return (
                     <Animated.View
                         key={item.id}
-                        style=style={this.state.position.getLayout()}
+                        style={this.setCardGestureStyle()}
                         {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
@@ -57,10 +100,7 @@ class Deck extends Component {
 
     render() {
         return (
-            <View
-                style=style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}
-            >
+            <View>
                 {this.renderCards()}
             </View>
         )
